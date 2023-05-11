bb34e7b0bc4093c4d77b85eafa5309e22fc86dd5
Tang Rou <99491612+Qual1dea@users.noreply.github.com>
2023-05-11 20:42:40 +0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BackgroundImage = Image.FromFile(@"C:\\Users\\Asus\\Desktop\\MafuMafu\\375f5df555cc3a150c4bb4013708c906.jpg");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            textBox1.Location = new Point(10, 10);  // координаты положения элемента на форме
+            textBox1.Size = new Size(100, 100);     //размеры элемента
+            textBox1.Width = 100;   //ширину элемента
+            textBox1.Height = 20;   //высота элемента
+            textBox1.Top = 10;      //вертикальную координату элемент
+            textBox1.Left = 10;     //горизонтальную координату элемента
+            textBox1.BackColor = Color.Black;   //цвет фона элемента
+            textBox1.ForeColor = Color.White;   //цвет текста в элементе
+            textBox1.Font = new Font("Time New Roman", 12, FontStyle.Bold); //шрифт и стиль текста
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.UseWaitCursor = false ; //тупая загрузка мыши
+            button1.FlatStyle = FlatStyle.Flat; //стиль кнопки button1 на "плоский"
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //double operand1 = double.Parse(textBox1.Text);
+            //double operand2 = double.Parse(textBox2.Text);
+            //string operation = textBox3.Text;
+
+            string operand1Str = textBox1.Text.Trim();  //Trim() удаляет любые начальные или конечные пробелы
+            string operand2Str = textBox2.Text.Trim();
+            string operation = textBox3.Text.Trim();
+            
+            double operand1 = double.Parse(operand1Str);    //Parse - это метод  преобразует строковое в числовое
+            double operand2 = double.Parse(operand2Str);
+            operation = operation.Replace("x", "*"); //заменяет  "x" на "*"
+
+            double result;  
+
+            if (operation == "+")
+            {
+                result = operand1 + operand2;
+            }
+            else if (operation == "-")
+            {
+                result = operand1 - operand2;
+            }
+            else if (operation == "*")
+            {
+                result = operand1 * operand2;
+            }
+            else if (operation == "/")
+            {
+                if (operand2 == 0)
+                {
+                    MessageBox.Show("Деление на ноль невозможно.");
+                    return;
+                }
+                result = operand1 / operand2;
+            }
+            else
+            {
+                MessageBox.Show("Некорректная операция.");
+                return;
+            }
+
+            label1.Text = result.ToString();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InputNumber1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(e.KeyChar >= '0' && e.KeyChar <= '9' || e.KeyChar >= '+' && e.KeyChar <= '-' || e.KeyChar == Convert.ToChar(8)))
+                e.KeyChar = (char)0;
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!(e.KeyCode >= Keys.D0 && e.KeyCode <= Keys.D9 || e.KeyCode >= Keys.Oemplus && e.KeyCode <= Keys.OemMinus || e.KeyCode == Keys.Back))
+                e.SuppressKeyPress = true;               
+        }
+
+        private void CheckButton_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(InputNumber1.Text))
+            {
+                e.Cancel = true;
+                InputNumber1.Focus();
+                errorProvider1.SetError(InputNumber1, "Введите число");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(InputNumber1, null);
+            }
+        }
+
+        private void CheckButton_Click(object sender, EventArgs e)
+        {
+            if (ValidateChildren(ValidationConstraints.Enabled))
+            {
+                MessageBox.Show(InputNumber1.Text, "Окно заголовка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sbyte result = Convert.ToSByte(InputNumber1.Text);
+                MessageBox.Show("Значение корректно"+result);
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Неверный формат");
+                InputNumber1.Focus();
+            }
+            catch(OverflowException)    
+            {
+                MessageBox.Show("Значение слишком большое/маленькое");
+                InputNumber1.Text = "0";
+            }
+        }
+
+        private void moveLeftButton_Click(object sender, EventArgs e)
+        {
+            int pixelIsToMove = -25;
+            textBoxLeft.Left = pixelIsToMove;
+        }
+    }
+}
